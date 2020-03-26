990658fec7c4df2b3fca203695c8c866821292f2
strupiech <strupiech98@gmail.com>
2020-03-26 13:58:35 +0100

    dodanie pól tak aby były zgodne z bazą danych

--- Cw3/Cw3/Models/Student.cs
+++ Cw3/Cw3/Models/Student.cs
@@ -2,9 +2,11 @@
 {
     public class Student
     {
-        public int IdStudent { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string IndexNumber { get; set; }
+        public string BirthDate { get; set; }
+        public string Semester { get; set; }
+        public string StudiesName { get; set; }
     }
 }
